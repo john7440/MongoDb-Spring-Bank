3d5e420f495bf5709cc33c64a66f54aec6ba03a1
Jonathan M. <neodev7440@gmail.com>
2026-06-18 14:39:53 +0200

    ajout du service pour les tansaction + performDeposit()

---
+++ src/main/java/fr/fms/mongodb_bank/services/TransactionService.java
@@ -0,0 +1,44 @@
+package fr.fms.mongodb_bank.services;
+
+import fr.fms.mongodb_bank.entities.BankAccount;
+import fr.fms.mongodb_bank.entities.Deposit;
+import fr.fms.mongodb_bank.repositories.BankAccountRepository;
+import fr.fms.mongodb_bank.repositories.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+    private final BankAccountRepository bankAccountRepository;
+
+    public TransactionService(TransactionRepository transactionRepository, BankAccountRepository bankAccountRepository) {
+        this.transactionRepository = transactionRepository;
+        this.bankAccountRepository = bankAccountRepository;
+    }
+
+    public boolean performDeposit(String accountId, double amount, String paymentMethod) {
+        Optional<BankAccount> optAccount = bankAccountRepository.findById(accountId);
+        if (optAccount.isEmpty()) return false;
+
+        BankAccount account = optAccount.get();
+
+        account.setBalance(account.getBalance() + amount);
+        bankAccountRepository.save(account);
+
+        Deposit deposit = new Deposit();
+        deposit.setSourceAccountId(accountId);
+        deposit.setAmount(amount);
+        deposit.setTransactionDate(LocalDateTime.now());
+        deposit.setDescription("Counter Deposit");
+        deposit.setPaymentMethod(paymentMethod);
+        transactionRepository.save(deposit);
+
+        return true;
+    }
+
+
+}
