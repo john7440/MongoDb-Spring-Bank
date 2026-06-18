0de1a010f200dc97f2a9ac1ac92cca9b72359ddc
Jonathan M. <neodev7440@gmail.com>
2026-06-18 08:30:48 +0200

    ajout de la class Customer avec annotations lombok

---
+++ src/main/java/fr/fms/mongodb_bank/entities/Customer.java
@@ -0,0 +1,20 @@
+package fr.fms.mongodb_bank.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "customers")
+public class Customer {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+}
