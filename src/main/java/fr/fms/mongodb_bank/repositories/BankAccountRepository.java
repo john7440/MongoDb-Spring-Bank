f8f78e435c0f05d2f4ca9b2cad100ae63bb943df
Jonathan M. <neodev7440@gmail.com>
2026-06-19 09:25:21 +0200

    suppression requete jpa non utilisée

--- src/main/java/fr/fms/mongodb_bank/repositories/BankAccountRepository.java
+++ src/main/java/fr/fms/mongodb_bank/repositories/BankAccountRepository.java
@@ -4,9 +4,6 @@ import fr.fms.mongodb_bank.entities.BankAccount;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BankAccountRepository extends MongoRepository<BankAccount,String> {
-    List<BankAccount> findByCustomerId(String customerId);
 }
